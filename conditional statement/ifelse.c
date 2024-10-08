22bfb35bc0c5421d83f77bf9539e054d0f7dcb7e
dheeraj <dhirajjoshi.987@gmail.com>
2024-10-08 08:04:44 +0530

    ifelse learning

---
+++ conditional statement/ifelse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 19;  // Initialize the age variable
+
+    // Check if the person is an adult
+    if (age >= 18) {
+        printf("You are an adult.\n");  // Print message if the age is 18 or above
+    }
+    else {
+        printf("You are not an adult.\n");  // Print message if the age is below 18
+    }
+
+    return 0;  // Return 0 to indicate successful program termination
+}
