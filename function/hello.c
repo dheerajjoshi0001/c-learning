26318f6d5da119979f9dcd75defd0514d42d4956
dheeraj <dhirajjoshi.987@gmail.com>
2025-02-23 22:55:40 +0530

    json

---
+++ function/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Function declaration/prototype
+void printHello();
+
+int main() {
+
+    // Function call
+    printHello();
+
+    return 0;
+}
+
+// Function definition
+void printHello() {
+    printf("Hello!\n");
+}
